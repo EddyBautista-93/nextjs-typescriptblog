34ddc6084e87fb8690bb7e04e2609bd8b4013c87
EddyBautista <bautista.j.eddy@gmail.com>
2022-09-03 20:47:32 -0500

    set up project with pulled example - pre typescript changes

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Howdy, I'm Eddy Bautista, I am a full stack developer for my city's transit agency!<br/>
+           I love to build and learn!
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
